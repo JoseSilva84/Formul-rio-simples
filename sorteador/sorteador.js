9eb774def9639613f7fd5ebc9201410a257491b1
José Evangelista da Silva Filho <juniornyanata@gmail.com>
2025-08-28 18:17:05 -0300

    7

--- sorteador/sorteador.js
+++ sorteador/sorteador.js
@@ -1,11 +1,20 @@
 const nomes = [];
 
-// const buttonAdicionar = document.querySelector("#sorteioAdicionar");
+const buttonAdicionar = document.querySelector("#sorteioAdicionar");
 
-const adicionar = (onclick) => {
-    buttonAdicionar.addEventListener("click", adicionar);
+const adicionar = (event) => {
+    event.preventDefault();
     const nome = document.querySelector("#nome").value;
     nomes.push(nome);
     console.log(nomes);
-}
+    let listaNomes = document.createElement("ul");
+    for (let i = 0; i < nomes.length; i++) {
+        let item = document.createElement("li");
+        item.textContent = nomes[i];
+        listaNomes.appendChild(item);
+    }
 
+    document.body.appendChild(listaNomes);
+};
+
+buttonAdicionar.addEventListener("click", adicionar);
