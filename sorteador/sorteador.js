a29b3516df78dd536e46abcbf7ba8dd1a44cfe07
José Evangelista da Silva Filho <juniornyanata@gmail.com>
2025-08-31 08:52:45 -0300

    11

--- sorteador/sorteador.js
+++ sorteador/sorteador.js
@@ -2,20 +2,23 @@ const nomes = [];
 
 const buttonAdicionar = document.querySelector("#sorteioAdicionar");
 const sortear = document.querySelector("#sortear");
-// const listaNomes = document.querySelector("#listaNomes");
-// listaNomes.innerHTML = "";
 
 const adicionar = (event) => {
     event.preventDefault();
     const nome = document.querySelector("#nome").value;
-    nomes.push(nome);
-    console.log(nomes);
-    let listaNomes = document.createElement("ul");
-    listaNomes.textContent = nomes[nomes.length - 1];
-    document.body.appendChild(listaNomes);
-    listaNomes.style.textAlign = "center";
-    listaNomes.style.backgroundColor = "lightgray";
-    listaNomes.style.display = "block";
+    if (nome) {
+        nomes.push(nome);
+        console.log(nomes);
+        let listaNomes = document.createElement("ul");
+        listaNomes.textContent = nomes[nomes.length - 1];
+        document.body.appendChild(listaNomes);
+        listaNomes.style.textAlign = "center";
+        listaNomes.style.backgroundColor = "rgba(255, 255, 255, 0.5)";
+        listaNomes.style.display = "block";
+    }
+    else {
+        alert("Por favor, insira um nome.");
+    }
 };
 
 buttonAdicionar.addEventListener("click", adicionar);
