4212e6df2626f517d1955fab9219d94ea38ba4b3
José Evangelista da Silva Filho <juniornyanata@gmail.com>
2025-09-11 15:07:42 -0300

    17

--- for/forArray.js
+++ for/forArray.js
@@ -1,5 +1,7 @@
 const participante = document.getElementById('nome');
 const nomes = [];
+const cores = ['blue', 'red', 'green'];
+const styleAdicional = [{ textAlign: 'center', padding: '10px' }];
 const participante1 = document.getElementById('participante1');
 const participante2 = document.getElementById('participante2');
 const participante3 = document.getElementById('participante3');
@@ -8,13 +10,20 @@ document.querySelector('form').addEventListener('submit', function(event) {
     event.preventDefault();
     nomes.push(participante.value);
     participante1.innerText = nomes[0] || '';
+    participante1.style.color = cores[0];
+    participante1.style.textAlign = 'center';
     participante2.innerText = nomes[1] || '';
+    participante2.style.color = cores[1];
+    participante2.style.textAlign = 'center';
     participante3.innerText = nomes[2] || '';
+    participante3.style.color = cores[2];
+    participante3.style.textAlign = 'center';
     if (participante3.innerText !== '') {
         const successMessage = document.createElement('h2');
         successMessage.innerText = 'Dados preenchidos com sucesso!';
+        successMessage.style.textAlign = 'center';
+        successMessage.style.color = 'gray';
         document.body.appendChild(successMessage);
     };
-    console.log(nomes);
 });
 
