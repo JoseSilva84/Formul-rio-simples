d1c1c2240d474aab29cd388884580363999c4215
José Evangelista da Silva Filho <juniornyanata@gmail.com>
2025-09-18 19:08:52 -0300

    27

--- api/bible.js
+++ api/bible.js
@@ -8,6 +8,49 @@ const verso = document.getElementById('verso');
 const buscarVersiculoBtn = document.getElementById('buscarVersiculo');
 const resultDiv = document.getElementById('result');
 
+livro.innerHTML = `
+    <option value="selecao" selected>Selecione o livro</option>
+            <option value="GEN">Gênesis</option>
+            <option value="EXO">Êxodo</option>
+            <option value="LEV">Levítico</option>
+            <option value="NUM">Números</option>
+            <option value="DEU">Deuteronômio</option>
+            <option value="JOS">Josué</option>
+            <option value="JDG">Juízes</option>
+            <option value="RUT">Rute</option>
+            <option value="1SA">1 Samuel</option>
+            <option value="2SA">2 Samuel</option>
+            <option value="1KI">1 Reis</option>
+            <option value="2KI">2 Reis</option>
+            <option value="1CH">1 Crônicas</option>
+            <option value="2CH">2 Crônicas</option>
+            <option value="EZR">Esdras</option>
+            <option value="NEH">Neemias</option>
+            <option value="EST">Ester</option>
+            <option value="JOB">Jó</option>
+            <option value="PSA">Salmos</option>
+            <option value="PRO">Provérbios</option>
+            <option value="ECC">Eclesiastes</option>
+            <option value="SNG">Cânticos</option>
+            <option value="ISA">Isaías</option>
+            <option value="JER">Jeremias</option>
+            <option value="LAM">Lamentações</option>
+            <option value="EZK">Ezequiel</option>
+            <option value="DAN">Daniel</option>
+            <option value="HOS">Oseias</option>
+            <option value="JOL">Joel</option>
+            <option value="AMO">Amós</option>
+            <option value="OBA">Obadias</option>
+            <option value="JON">Jonas</option>
+            <option value="MIC">Miquéias</option>
+            <option value="NAM">Naum</option>
+            <option value="HAB">Habacuque</option>
+            <option value="ZEP">Sofonias</option>
+            <option value="HAG">Ageu</option>
+            <option value="ZAC">Zacarias</option>
+            <option value="MAL">Malaquias</option>
+`;
+
 secao.addEventListener('change', () => {
     if (secao.value === antigoTestamento.value) {
         livro.innerHTML = `
@@ -97,6 +140,7 @@ buscarVersiculoBtn.addEventListener('click', async (event) => {
     const {book_id, chapter, verse, text} = await apiUrl.json();
     const data = {book_id, chapter, verse, text};
 
+    console.log(data.book_id);
     // console.log(data.text);
     resultDiv.innerText = data.text;
 });
