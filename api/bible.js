921b561a3da15e5982ea43446d1292e5156ac4a5
José Evangelista da Silva Filho <juniornyanata@gmail.com>
2025-09-18 11:26:25 -0300

    25

--- api/bible.js
+++ api/bible.js
@@ -12,87 +12,93 @@ secao.addEventListener('change', () => {
     if (secao.value === antigoTestamento.value) {
         livro.innerHTML = `
             <option value="selecao1" selected>Selecione o livro</option>
-            <option value="Gênesis">Gênesis</option>
-            <option value="Êxodo">Êxodo</option>
-            <option value="Levítico">Levítico</option>
-            <option value="Números">Números</option>
-            <option value="Deuteronômio">Deuteronômio</option>
-            <option value="Josué">Josué</option>
-            <option value="Juízes">Juízes</option>
-            <option value="Rute">Rute</option>
-            <option value="1 Samuel">1 Samuel</option>
-            <option value="2 Samuel">2 Samuel</option>
-            <option value="1 Reis">1 Reis</option>
-            <option value="2 Reis">2 Reis</option>
-            <option value="1 Crônicas">1 Crônicas</option>
-            <option value="2 Crônicas">2 Crônicas</option>
-            <option value="Esdras">Esdras</option>
-            <option value="Neemias">Neemias</option>
-            <option value="Ester">Ester</option>
-            <option value="Jó">Jó</option>
-            <option value="Salmos">Salmos</option>
-            <option value="Provérbios">Provérbios</option>
-            <option value="Eclesiastes">Eclesiastes</option>
-            <option value="Cânticos">Cânticos</option>
-            <option value="Isaías">Isaías</option>
-            <option value="Jeremias">Jeremias</option>
-            <option value="Lamentações">Lamentações</option>
-            <option value="Ezequiel">Ezequiel</option>
-            <option value="Daniel">Daniel</option>
-            <option value="Oseias">Oseias</option>
-            <option value="Joel">Joel</option>
-            <option value="Amós">Amós</option>
-            <option value="Obadias">Obadias</option>
-            <option value="Jonas">Jonas</option>
-            <option value="Miquéias">Miquéias</option>
-            <option value="Naum">Naum</option>
-            <option value="Habacuque">Habacuque</option>
-            <option value="Sofonias">Sofonias</option>
-            <option value="Ageu">Ageu</option>
-            <option value="Zacarias">Zacarias</option>
-            <option value="Malaquias">Malaquias</option>
+            <option value="GEN">Gênesis</option>
+            <option value="EXO">Êxodo</option>
+            <option value="LEV">Levítico</option>
+            <option value="NUM">Números</option>
+            <option value="DEU">Deuteronômio</option>
+            <option value="JOS">Josué</option>
+            <option value="JDG">Juízes</option>
+            <option value="RUT">Rute</option>
+            <option value="1SA">1 Samuel</option>
+            <option value="2SA">2 Samuel</option>
+            <option value="1KI">1 Reis</option>
+            <option value="2KI">2 Reis</option>
+            <option value="1CH">1 Crônicas</option>
+            <option value="2CH">2 Crônicas</option>
+            <option value="EZR">Esdras</option>
+            <option value="NEH">Neemias</option>
+            <option value="EST">Ester</option>
+            <option value="JOB">Jó</option>
+            <option value="PSA">Salmos</option>
+            <option value="PRO">Provérbios</option>
+            <option value="ECC">Eclesiastes</option>
+            <option value="SNG">Cânticos</option>
+            <option value="ISA">Isaías</option>
+            <option value="JER">Jeremias</option>
+            <option value="LAM">Lamentações</option>
+            <option value="EZK">Ezequiel</option>
+            <option value="DAN">Daniel</option>
+            <option value="HOS">Oseias</option>
+            <option value="JOL">Joel</option>
+            <option value="AMO">Amós</option>
+            <option value="OBA">Obadias</option>
+            <option value="JON">Jonas</option>
+            <option value="MIC">Miquéias</option>
+            <option value="NAM">Naum</option>
+            <option value="HAB">Habacuque</option>
+            <option value="ZEP">Sofonias</option>
+            <option value="HAG">Ageu</option>
+            <option value="ZAC">Zacarias</option>
+            <option value="MAL">Malaquias</option>
         `;
     } else if (secao.value === novoTestamento.value) {
             livro.innerHTML = `
                 <option value="selecao2" selected>Selecione o livro</option>
-                <option value="Mateus">Mateus</option>
-                <option value="Marcos">Marcos</option>
-                <option value="Lucas">Lucas</option>
-                <option value="João">João</option>
-                <option value="Atos">Atos</option>
-                <option value="Romanos">Romanos</option>
-                <option value="1 Coríntios">1 Coríntios</option>
-                <option value="2 Coríntios">2 Coríntios</option>
-                <option value="Gálatas">Gálatas</option>
-                <option value="Efésios">Efésios</option>
-                <option value="Filipenses">Filipenses</option>
-                <option value="Colossenses">Colossenses</option>
-                <option value="1 Tessalonicenses">1 Tessalonicenses</option>
-                <option value="2 Tessalonicenses">2 Tessalonicenses</option>
-                <option value="1 Timóteo">1 Timóteo</option>
-                <option value="2 Timóteo">2 Timóteo</option>
-                <option value="Tito">Tito</option>
-                <option value="Filemom">Filemom</option>
-                <option value="Hebreus">Hebreus</option>
-                <option value="Tiago">Tiago</option>
-                <option value="1 Pedro">1 Pedro</option>
-                <option value="2 Pedro">2 Pedro</option>
-                <option value="1 João">1 João</option>
-                <option value="2 João">2 João</option>
-                <option value="3 João">3 João</option>
-                <option value="Judas">Judas</option>
-                <option value="Apocalipse">Apocalipse</option>
+                <option value="Mat">Mateus</option>
+                <option value="MRK">Marcos</option>
+                <option value="LUK">Lucas</option>
+                <option value="JHN">João</option>
+                <option value="ACT">Atos</option>
+                <option value="ROM">Romanos</option>
+                <option value="1CO">1 Coríntios</option>
+                <option value="2CO">2 Coríntios</option>
+                <option value="GAL">Gálatas</option>
+                <option value="EPH">Efésios</option>
+                <option value="PHP">Filipenses</option>
+                <option value="COL">Colossenses</option>
+                <option value="1TH">1 Tessalonicenses</option>
+                <option value="2TI">2 Tessalonicenses</option>
+                <option value="1TI">1 Timóteo</option>
+                <option value="2TI">2 Timóteo</option>
+                <option value="TIT">Tito</option>
+                <option value="PHM">Filemom</option>
+                <option value="HEB">Hebreus</option>
+                <option value="JAS">Tiago</option>
+                <option value="1PE">1 Pedro</option>
+                <option value="2PE">2 Pedro</option>
+                <option value="1JN">1 João</option>
+                <option value="1JN">2 João</option>
+                <option value="3JN">3 João</option>
+                <option value="JUD">Judas</option>
+                <option value="REV">Apocalipse</option>
             `;
     }
 });
 
 
-const apiUrl = 'https://bible-api.com/data/almeida';
-
-buscarVersiculoBtn.addEventListener('click', (event) => {
+buscarVersiculoBtn.addEventListener('click', async (event) => {
     event.preventDefault();
-    const livroValue = livro.value.trim();
-    if (livro.value) {
-        
-    }
+
+    const input = livro.value;
+
+    // const apiUrl = await 'https://bible-api.com/data/almeida/GEN/2';
+    const apiUrl = await fetch(`https://bible-api.com/data/almeida/${input}/${capitulo.value}`);
+
+    const {book_id, chapter, verse, text} = await apiUrl.json();
+    const data = {book_id, chapter, verse, text};
+
+    console.log(apiUrl);
+    console.log(data);
+    console.log(data.toString());
 });
