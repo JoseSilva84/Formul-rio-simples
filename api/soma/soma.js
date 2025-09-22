4e2ad533de74c2a89837510ca4681bf51d462ec0
José Evangelista da Silva Filho <juniornyanata@gmail.com>
2025-09-21 22:47:43 -0300

    soma

---
+++ api/soma/soma.js
@@ -0,0 +1,14 @@
+// function soma(x, y){
+//     return x + y;
+// }
+// const total = soma(5, 5);
+
+// console.log(total);
+
+const soma = (x, y) => {
+    return x + y;
+}
+
+const total = soma(5, 6);
+
+console.log(total);
